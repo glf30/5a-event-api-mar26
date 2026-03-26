cd16d908ad00666e5c860104251d28ac32a39bf0
glf30 <greyson.frazier@gmail.com>
2026-03-26 16:55:17 -0400

    Set up ability to create user

---
+++ routes/users/users-controller.js
@@ -0,0 +1,12 @@
+const User = require("./users-model");
+
+const createUser = async (userData) => {
+    try {
+        const newUser = await User.create(userData);
+        return newUser
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = { createUser };
